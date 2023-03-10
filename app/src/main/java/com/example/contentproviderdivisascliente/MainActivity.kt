3c9928e77eaa3a7cd97c968f0d797b572e850008
JulioZL <zavala.lopez.julioisaac@gmail.com>
2023-03-10 16:17:26 -0600

    App Terminada.

--- app/src/main/java/com/example/contentproviderdivisascliente/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderdivisascliente/MainActivity.kt
@@ -8,9 +8,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.SimpleCursorAdapter
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
@@ -29,7 +32,6 @@ class MainActivity : AppCompatActivity() {
 
         override fun onLoaderReset(loader: Loader<Cursor>) {
             //TODO("Not yet implemented")
-            Log.i("this_app", " Nada de CP")
         }
 
         @SuppressLint("Range")
@@ -54,8 +56,12 @@ class MainActivity : AppCompatActivity() {
                 val text2Value = selectedCursor.getString(selectedCursor.getColumnIndex("valor"))
                 txt.text = text2Value
 
+                val selectedCursor2 = spn2.selectedItem as Cursor
+                val text2Value2 = selectedCursor2.getString(selectedCursor.getColumnIndex("valor"))
+                txt.text = text2Value2
+
                 while (moveToNext()){
-                    Log.i("this_app", " id: ${getInt(0)} , code: ${getString(1)}, moneda ${getString(2)}")
+                    //Log.i("this_app", " id: ${getInt(0)} , code: ${getString(1)}, moneda ${getString(2)}")
                 }
             }
 
@@ -63,19 +69,33 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    lateinit var btn:Button
+
     lateinit var txt:TextView
+    lateinit var txt2:TextView
+
+    lateinit var op:EditText
 
 
     lateinit var spn : Spinner
     lateinit var spn2 : Spinner
+    lateinit var aux : Spinner
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.cambiar)
 
         spn = findViewById(R.id.spinner)
         spn2 = findViewById(R.id.spinner2)
 
         txt = findViewById(R.id.textView)
+        txt2 = findViewById(R.id.textView2)
+
+        op = findViewById(R.id.variable)
+
+        spn2.isEnabled=false
 
         LoaderManager.getInstance(this)
             .initLoader<Cursor>(1001, null, mLoaderCallbacks)
@@ -86,7 +106,7 @@ class MainActivity : AppCompatActivity() {
         )
         micursor?.apply {
             while (moveToNext()){
-                Log.i("this_app", " id: ${getInt(0)} , code: ${getString(1)}, moneda ${getString(2)}")
+                //Log.i("this_app", " id: ${getInt(0)} , code: ${getString(1)}, moneda ${getString(2)}")
             }
         }
 
@@ -100,6 +120,99 @@ class MainActivity : AppCompatActivity() {
 
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // No se seleccionó ningún elemento
+            }
+        }
+        spn2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            @SuppressLint("Range")
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedCursor = parent.getItemAtPosition(position) as Cursor
+                val text2Value = selectedCursor.getString(selectedCursor.getColumnIndex("valor"))
+                txt2.text = text2Value
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // No se seleccionó ningún elemento
+            }
+        }
+
+        op.doOnTextChanged { text, start, before, count ->
+            operacion()
+        }
+
+        btn.setOnClickListener {
+            if(spn.isEnabled){
+                spn.isEnabled=false
+                spn2.isEnabled=true
+            }
+            else{
+                spn2.isEnabled=false
+                spn.isEnabled=true
+            }
+            val pos1 = spn.selectedItemPosition
+            val pos2 = spn2.selectedItemPosition
+            val adapter1 = spn.adapter
+            val adapter2 = spn2.adapter
+            spn.adapter = adapter2
+            spn2.adapter = adapter1
+            spn.setSelection(pos2)
+            spn2.setSelection(pos1)
+
+            operacion()
+        }
+    }
+
+    fun operacion(){
+
+        val vari:EditText
+        vari=findViewById(R.id.variable)
+
+        val aux1:String
+        if(op.getText().toString().isEmpty()){
+            aux1 = "0"
+        }
+        else{
+            if(spn2.isEnabled){
+                //Funcion de cambio de divisas
+
+                aux1 = vari.text.toString()
+                val aux2 = txt2.text.toString()
+
+                var x:Double
+                var y:Double
+                var resultado:Double
+                x = aux1.toDouble()
+                y = aux2.toDouble()
+
+                resultado=x*y
+
+                val resul:EditText
+                resul=findViewById(R.id.resultado)
+
+                resul.setText(resultado.toString())
+            }
+            else{
+                //Funcion de cambio de divisas
+                val vari:EditText
+                vari=findViewById(R.id.variable)
+
+                val multiplicador = vari.text.toString()
+                val dividendo = txt.text.toString()
+
+                var x:Double
+                var y:Double
+                var resultado:Double
+
+                x = multiplicador.toDouble()
+                y = dividendo.toDouble()
+
+                var a:Double
+                a=1.0
+                resultado=(a/y)*x
+
+                val resul:EditText
+                resul=findViewById(R.id.resultado)
+
+                resul.setText("$ "+resultado.toString())
             }
         }
     }
